c38fe7c2c119fbfaed7fde354d9e4bb0558e93db
Can Yilmaz <spawnedc@gmail.com>
2021-07-06 21:32:42 +0100

    Remove unused things

--- src/data/doubleShot.gd
+++
@@ -1,6 +0,0 @@
-extends Weapon
-
-var weapon_name = 'Double Shot'
-var bullet_scene_name = 'SingleShot.tscn'
-var bullet_count = 2
-var fire_rate = 0.5
--- src/data/weapon.gd
+++
@@ -1,8 +0,0 @@
-extends Node2D  # Inherits from the Global Node2D class
-
-class_name Weapon
-
-var weapon_name: String
-var bullet_scene_name: String
-var bullet_count: int
-var fire_rate: float
