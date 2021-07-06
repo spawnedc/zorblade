036dbe5b0c12801eebd91887df3de4f9d2c4eef9
Can Yilmaz <spawnedc@gmail.com>
2021-07-06 21:40:25 +0100

    Add some style

--- src/scripts/WeaponManager.gd
+++ src/scripts/WeaponManager.gd
@@ -12,4 +12,3 @@ func _get_weapon_data(weapon_name: String):
 func set_weapon(weapon_name: String):
 	weapon_data = _get_weapon_data(weapon_name)
 	emit_signal('weapon_change', weapon_data)
-	# bullet_scene = load('res://scenes/weapons/' + weapon_data['bullet_scene'] + '.tscn')
