a53b6283ed7f8b85e554e513462678540fac4e6c
Can Yilmaz <spawnedc@gmail.com>
2021-08-08 16:30:20 +0100

    Add fullscreen toggle button

--- src/scenes/root.gd
+++ src/scenes/root.gd
@@ -8,4 +8,8 @@ func _ready():
 	SceneManager.connect("change_scene", sceneLoader, "goto_scene")
 	MusicManager.connect("stop_playback", musicPlayer, "stop_music")
 	MusicManager.connect("play_music", musicPlayer, "play_music")
-	# musicPlayer.play_music('SkyFire/SkyFire (Title Screen)')
+
+
+func _unhandled_input(_event):
+	if Input.is_action_just_pressed("toggle_fullscreen"):
+		OS.set_window_fullscreen(! OS.window_fullscreen)
