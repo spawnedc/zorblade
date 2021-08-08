9666b8d31e80cef55f2b0448e15d6ddd7476cf26
Can Yilmaz <spawnedc@gmail.com>
2021-08-08 22:18:24 +0100

    Cleanup

--- src/states/SpwGames/SpwGames.gd
+++ src/states/SpwGames/SpwGames.gd
@@ -1,19 +1,9 @@
 extends Node
 
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
 
-
-# Called when the node enters the scene tree for the first time.
 func _ready():
 	$Timer.connect("timeout", self, "_on_timeout")
-	MusicManager.play('SkyFire/SkyFire (Title Screen).ogg')
 
 
 func _on_timeout():
 	SceneManager.goto_scene("Title")
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
